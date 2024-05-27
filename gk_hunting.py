517082255e81964eac309da04dc9363da800e3e7
Lorenzo Barzan <96786579+Frizzin0@users.noreply.github.com>
2024-05-27 17:13:49 +0200

    Update gk_hunting.py

--- gk_hunting.py
+++ gk_hunting.py
@@ -1,8 +1,15 @@
+pip install playwright --upgrade
+pip install selenium --upgrade
+pip install BeautifulSoup --upgrade
 
-import streamlit as st
-import time
+from playwright.sync_api import sync_playwright
 import pandas as pd
+import time
+from tqdm import tqdm
+import re
 
+
+#selenium components
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -10,12 +17,21 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
-from multiprocessing import Pool
+
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+options.add_experimental_option(
+        "prefs", {
+            # block image loading
+            "profile.managed_default_content_settings.images": 2,
+        }
+    )
+
 
 
 def enlist_restaurants(url):  
     # Initialize the WebDriver (e.g., Chrome)
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
     try:
         # Open the webpage
@@ -30,13 +46,14 @@ def enlist_restaurants(url):
         except Exception as e:
             print("No cookie pop-up detected or unable to close it:", e)
 
-        # Wait for the main button to be clickable and click it
+        # Handle the popup after accepting cookies
         try:
-            main_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.ccl-b91164e9bf573ff0')))
-            main_button.click()
-            print("Main button clicked.")
+            popup_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, 'ccl-388f3fb1d79d6a36.ccl-6d2d597727bd7bab.ccl-59eced23a4d9e077.ccl-7be8185d0a980278')))
+            popup_button.click()
+            time.sleep(1)  # Adjust this time if necessary
+            print("Popup button clicked.")
         except Exception as e:
-            print("Main button not found or not clickable:", e)
+            print("No popup detected or unable to close it:", e)
 
         # Wait for the elements to load after clicking the button
         time.sleep(3)  # Adjust the sleep time as necessary or use more sophisticated waits
@@ -48,7 +65,7 @@ def enlist_restaurants(url):
         soup = BeautifulSoup(page_source, 'html.parser')
 
         # Find all list elements with the specified class
-        list_items = soup.find_all('li', class_='home-map__card-spacer HomeMapCards-67670a91a1d402aa')
+        list_items = soup.find_all('li', class_='HomeFeedGrid-b0432362335be7af')
 
         # Extract href and aria-label from the desired elements within each list item
         result = []
@@ -62,106 +79,121 @@ def enlist_restaurants(url):
         # Create a DataFrame from the result list
         df = pd.DataFrame(result)
 
-        # Print the DataFrame
 
     finally:
         # Close the WebDriver
         driver.quit()
 
-    return df
+        return df
+
+
+def handle_dialog(dialog):
+    if "non può ricevere ordini" in dialog.message:
+        print(f'clicking "Yes" to {dialog.message}')
+        dialog.accept()  # press "Yes"
+    else:
+        dialog.dismiss()  # press "No"
 
 
 def collect_info_on_restaurants(df):
-    count = 0
-    for res in tqdm(df['href'][0:20]):
-        # Initialize WebDriver (this will automatically download the correct version of ChromeDriver)
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    all_res = []
 
-        # Define the URL to test
-        url = res  # Replace with the actual URL from your DataFrame
-
-        # Open the URL
-        driver.get(url)
-
-        # Wait for the page to load completely
-        time.sleep(5)  # Adjust this time as necessary
-
-        try:
-            # Handle the cookie pop-up if it appears
-            # Accept cookies if the popup is present
-            try:
-                cookie_button = WebDriverWait(driver, 8).until(EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "Accetta") or contains(text(), "accetto")]')))
-                cookie_button.click()
-                time.sleep(1)  # Adjust this time if necessary
-                print("Cookie button clicked.")
-            except Exception as e:
-                print("No cookie pop-up detected or unable to close it:", e)
-
-            
-            #Handle "Restaurant can't accept orders"
-            #time.sleep(1)
-            #try:
-            #    close_button = WebDriverWait(driver, 8).until(EC.element_to_be_clickable((By.CLASS_NAME, "ccl-388f3fb1d79d6a36.ccl-9ed29b91bb2d9d02.ccl-59eced23a4d9e077.ccl-7be8185d0a980278")))
-            #    close_button.click()
-            #    time.sleep(1)  # Adjust this time if necessary
-            #    print("Close button clicked.")
-            #except Exception as e:
-            #    print("Close button for 'Restaurant can't accept orders' pop-up not found:", e)
+    for res in tqdm(df['href']):
         
+        # Running Playwright & open website
+        playwright = sync_playwright().start()
+        browser = playwright.chromium.launch(headless=False)
+        page = browser.new_page()
+        page.on("dialog", lambda dialog: handle_dialog(dialog))
 
-                
-            # Locate the parent element that contains the button
-            parent_element = driver.find_element(By.CLASS_NAME, "UIHeaderInfoRows-023104015d7355b8.ccl-a5e1512b87ef2079")
-            
-            # Find and click the button within this parent element
-            button = parent_element.find_element(By.CLASS_NAME, "ccl-4704108cacc54616.ccl-4f99b5950ce94015.ccl-724e464f2f033893")
-            button.click()
-            
-            # Wait for the pop-up to load
-            time.sleep(1)  # Adjust this time as necessary
-            
-            # Find the parent element of the popup
-            popup_element = driver.find_element(By.CLASS_NAME, "ccl-9237ff225b0d2789.ccl-e770a464a394226e.ccl-bae50209245d2304")
+
+        page.goto(res, wait_until='networkidle')
+        time.sleep(0.5)
+        data_item = ""
         
-            # Find all elements with the class name "UIContentCard-32d54d142ca96f5c" within the popup
-            link_elements = popup_element.find_elements(By.CLASS_NAME, "UIContentCard-32d54d142ca96f5c")
-            
-            # Extract the href attribute from the second link element
-            if len(link_elements) >= 2:
-                link = link_elements[1].get_attribute("href")
-                df.loc[df['href']==res, 'Link'] = link
-                
-                # Find all elements with the class name "UILines-eb427a2507db75b3" within the popup
-                text_elements = popup_element.find_elements(By.CLASS_NAME, "UILines-eb427a2507db75b3")
-                
-                # Extract the text from the second text element
-                tex = ''
-                for t in text_elements:
-                    tex = tex + '^' + t.text
-                aux.append(tex)
-                df.loc[df['href']==res, 'text'] = tex
-            else:
-                print("Second link element not found")
+        page.get_by_text("Allergeni e tanto altro").click()
+        items = page.locator('//span[@class="ccl-649204f2a8e630fd ccl-6f43f9bb8ff2d712"]').all()
+        phones = page.locator('//span[@class="ccl-649204f2a8e630fd ccl-98a86d2cf2dd0739"]').all()
+        
+        for item in items:
+            data_item += "^" + item.inner_text()
 
-        except Exception as e:
-            print("Error:", e)
+        for phone in phones:
+            data_item += "^" + phone.inner_text()
 
-        # Close the WebDriver
-        count += 1
-        st.write(count)
-        driver.quit()
+        all_res.append(data_item)
+        
+        #
+        browser.close()
+        playwright.stop()
 
+    #print(len(all_res))
+    #print(all_res)
+    df['text'] = all_res
+    
+    return df
+
+
+
+def data_cleaning(df):
+    df['text'] = df['text'].map(lambda x: str(x).split('^')[1:])
+
+    def extract_phone_number(text_list):
+        phone_pattern = re.compile(r'\+?\d[\d -]{8,12}\d')
+        for text in text_list:
+            match = phone_pattern.search(text)
+            if match:
+                return match.group()
+        return None
+
+
+    def extract_address(text_list):
+        address_pattern = re.compile(r'.*[a-zA-Z].*\d{5}$')
+        for text in text_list:
+            if address_pattern.match(text) and not text.startswith("Chiama"):
+                return text
+        return None
+
+    df['phone_number'] = df['text'].apply(extract_phone_number)
+    df['address'] = df['text'].apply(extract_address)
+
+    # Function to handle the aggregation logic
+    def aggregate_column(series):
+        if series.nunique() == 1:
+            return series.iloc[0]
+        else:
+            return series.unique().tolist()
+
+    #Select only the columns needed for aggregation
+    columns_to_aggregate = ['href', 'aria-label', 'text', 'phone_number']
+
+    # Group by "address" and aggregate
+    grouped = df.groupby('address')[columns_to_aggregate].agg({
+        'text': 'count',  # Count the number of occurrences for "n°"
+        'aria-label': aggregate_column,
+        'href': aggregate_column,
+        'phone_number': aggregate_column
+    }).reset_index()
+
+    # Add the 'text' column with a custom aggregation
+    grouped['description'] = df.groupby('address')['text'].apply(lambda x: ', '.join(map(str, x))).values
+
+    # Rename the columns as needed
+    grouped.rename(columns={'text': 'n°'}, inplace=True)
+    # Sort the DataFrame by the "n°" column
+    grouped = grouped.sort_values(by='n°', ascending=False)
+    return grouped
 
 def main():
 
-    st.title("Ghostkitchen Hunting")
-    url = st.text_input("Enter the URL:", "https://deliveroo.it/it/restaurants/cosenza/cosenza/?fulfillment_method=COLLECTION&geohash=sqgx9exh8hsc&delivery_time=1716402600")
+    print("input url")
+    url = input()
+    df = enlist_restaurants(url)
+    df = collect_info_on_restaurants(df)
+    df = data_cleaning(df)
 
-    if st.button("Fetch Data"):
-        df = enlist_restaurants(url)
-        df = collect_info_on_restaurants(df)
-    
-    st.write(df)
+    print("output file name")
+    output = input()
+    df.to_excel(output)
 
-if __name__ == "__main__":
-    main()
+main()
